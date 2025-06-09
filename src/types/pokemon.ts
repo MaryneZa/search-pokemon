157ebeade95a2b8e0644da91d0fda0a736db2b43
MaryneZa <maryne6916@gmail.com>
2025-06-09 23:10:06 +0700

    pokemon interface

---
+++ src/types/pokemon.ts
@@ -0,0 +1,32 @@
+export interface Pokemon {
+  id: string;
+  number?: string;
+  name?: string;
+  classification?: string;
+  types?: string[];
+  resistant?: string[];
+  weaknesses?: string[];
+  fleeRate?: number;
+  maxCP?: number;
+  maxHP?: number;
+  image?: string;
+  weight?: {
+    minimum?: string;
+    maximum?: string;
+  };
+  height?: {
+    minimum?: string;
+    maximum?: string;
+  };
+  attacks?: {
+    fast?: Attack[];
+    special?: Attack[];
+  };
+  evolutions?: Pokemon[];
+}
+
+export interface Attack {
+  name?: string;
+  type?: string;
+  damage?: number;
+}
