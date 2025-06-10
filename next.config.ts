d20a3455b7308b4b8aac458a2a6d5882c277c0a0
MaryneZa <maryne6916@gmail.com>
2025-06-10 22:31:41 +0700

    setup /home page

--- next.config.ts
+++ next.config.ts
@@ -2,6 +2,15 @@ import type { NextConfig } from "next";
 
 const nextConfig: NextConfig = {
   /* config options here */
+  async redirects() {
+    return [
+      {
+        source: '/', // Root path
+        destination: '/home', // Redirect to /home
+        permanent: false, // Use temporary redirect (status code 307)
+      },
+    ];
+  },
   images: {
     domains: ["img.pokemondb.net"],
   },
