2c87fc9c4da7d398af2e185381a07fedd82395e9
MaryneZa <maryne6916@gmail.com>
2025-06-11 00:31:39 +0700

    fix typo bugs

--- src/components/layout/apollo-wrapper.tsx
+++ src/components/layout/apollo-wrapper.tsx
@@ -1,8 +1,8 @@
 'use client';
 
 import { ApolloProvider } from '@apollo/client';
-import {client} from '@/lib/apolloClient';
+import {apolloClient} from '@/lib/apolloClient';
 
 export default function ApolloWrapper({ children }: { children: React.ReactNode }) {
-  return <ApolloProvider client={client}>{children}</ApolloProvider>;
+  return <ApolloProvider client={apolloClient}>{children}</ApolloProvider>;
 }
